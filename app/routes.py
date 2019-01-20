2f7266f4b66c514e645a2396a213ae82c9721072
Eugene Zalivadnyi <zalik132@gmail.com>
2019-01-20 16:43:40 +0200

    added base.html and index.html Templates (Chapter 2)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Eugene'}
+    posts = [
+        {
+            'author': {'username': "John Lennon"},
+            'body': "Let's see how it works."
+        },
+        {
+            'author': {'username': 'Britney Spears'},
+            'body': "Oh baby, baby! I'm rock!"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
