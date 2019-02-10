762830322911662495567f1dd49e4d30ea72c9a1
Eugene Zalivadnyi <zalik132@gmail.com>
2019-02-10 13:16:50 +0200

    added Flask-Migrate and db configs

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'microblog_sqlite.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
