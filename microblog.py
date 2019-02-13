3d24b5ffd9c1acb0a0073a354f1a4afc05105305
Eugene Zalivadnyi <zalik132@gmail.com>
2019-02-13 16:12:24 +0200

    Automatically add db and models to flask shell context.

--- microblog.py
+++ microblog.py
@@ -1,1 +1,13 @@
-from app import app
+from application import application_instance, db
+from application.models import Post, User
+
+
+# Automatically add objects from dictionary to shell context.
+# Fired with command 'flask shell'.
+@application_instance.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
