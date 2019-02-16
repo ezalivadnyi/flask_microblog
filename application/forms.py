f22c01c313cc84f923eee7282e625b65f142af92
Eugene Zalivadnyi <zalik132@gmail.com>
2019-02-16 20:32:49 +0200

    Duplicate Username Bugfix

--- application/forms.py
+++ application/forms.py
@@ -37,3 +37,13 @@ class EditProfileForm(FlaskForm):
     facebook = StringField('Facebook username (will be added after https://www.facebook.com/...)', validators=[Length(max=140)])
     about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Save')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('This username is already in use. Please chose another.')
