8b9344005e5454d4f921dc465bcef41f9bbd78a5
Eugene Zalivadnyi <zalik132@gmail.com>
2019-02-16 19:00:53 +0200

    added Logging errors to file

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import logging
+import os
 
 application_instance = Flask(__name__)
 application_instance.config.from_object(Config)
@@ -13,8 +14,8 @@ migrate = Migrate(application_instance, db)
 login = LoginManager(application_instance)
 login.login_view = 'login'
 
-# Logging errors by email
 if not application_instance.debug:
+    # Logging errors by email
     if application_instance.config['MAIL_SERVER']:
         auth = None
         if application_instance.config['MAIL_USERNAME'] or application_instance.config['MAIL_PASSWORD']:
@@ -32,5 +33,17 @@ if not application_instance.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         application_instance.logger.addHandler(mail_handler)
+
+    # Logging errors to file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_hadler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_hadler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_hadler.setLevel(logging.INFO)
+    application_instance.logger.addHandler(file_hadler)
+    application_instance.logger.setLevel(logging.INFO)
+    application_instance.logger.info('Microblog startup')
 
 from application import routes, models, errors
